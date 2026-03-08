99a3524b6541ec32b722c7293213969563ba145c
Dijash <utsavbhattarai29@gmail.com>
2026-03-08 23:51:58 +0545

    Admin Page

---
+++ js/Admin.js
@@ -0,0 +1,9 @@
+  function openSidebar() {
+    document.getElementById('sidebar').classList.add('open');
+    document.getElementById('overlay').classList.add('open');
+  }
+
+  function closeSidebar() {
+    document.getElementById('sidebar').classList.remove('open');
+    document.getElementById('overlay').classList.remove('open');
+  }
